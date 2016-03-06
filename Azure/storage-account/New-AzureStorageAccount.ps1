d5ad4b68c7de8e6e4134a498bded5530159cf2ab
Matt D <matthewdavis111@gmail.com>
2016-03-06 18:18:14 +0000

    added URI for dev, research and test env

--- Azure/storage-account/New-AzureStorageAccount.ps1
+++ Azure/storage-account/New-AzureStorageAccount.ps1
@@ -3,10 +3,12 @@
 $deployname = "storageAcctTemplate"
 $rgname = "research-resource-group-Matt"
 $TemplateUri = "https://raw.githubusercontent.com/MatthewJDavis/PowerShell/azure-storage-accounts/Azure/storage-account/storage-account-template/azuredeploy.json"
-$TemplateParameterUri = "https://raw.githubusercontent.com/MatthewJDavis/PowerShell/azure-storage-accounts/Azure/storage-account/storage-account-template/azuredeploy.parameters.json"
+$ResTemplateParameterUri = "https://raw.githubusercontent.com/MatthewJDavis/PowerShell/azure-storage-accounts/Azure/storage-account/storage-account-template/azuredeploy.parameters.json"
+$TestTemplateParameterUri = "https://raw.githubusercontent.com/MatthewJDavis/PowerShell/azure-storage-accounts/Azure/storage-account/storage-account-test/storage-acct-parameters-test.json"
+$DevTemplateParameterUri = "https://raw.githubusercontent.com/MatthewJDavis/PowerShell/azure-storage-accounts/Azure/storage-account/storage-account-test/storage-acct-parameters-test.json"
 
 
-New-AzureRmResourceGroupDeployment -Name $deployname -ResourceGroupName $rgname -TemplateUri $TemplateUri -TemplateParameterUri $TemplateParameterUri
+New-AzureRmResourceGroupDeployment -Name $deployname -ResourceGroupName $rgname -TemplateUri $TemplateUri -TemplateParameterUri $TemplateParameterUri -Verbose
 
 
 
