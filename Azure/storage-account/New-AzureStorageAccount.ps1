c4718a5c8846a6bebcd330fcecd740a0dd1a0620
Matt D <matthewdavis111@gmail.com>
2016-03-06 19:29:44 +0000

    Corrected URI address

--- Azure/storage-account/New-AzureStorageAccount.ps1
+++ Azure/storage-account/New-AzureStorageAccount.ps1
@@ -3,12 +3,12 @@
 $deployname = "storageAcctTemplate"
 $rgname = "research-resource-group-Matt"
 $TemplateUri = "https://raw.githubusercontent.com/MatthewJDavis/PowerShell/azure-storage-accounts/Azure/storage-account/storage-account-template/azuredeploy.json"
-$ResTemplateParameterUri = "https://raw.githubusercontent.com/MatthewJDavis/PowerShell/azure-storage-accounts/Azure/storage-account/storage-account-template/azuredeploy.parameters.json"
+$ResTemplateParameterUri = "https://raw.githubusercontent.com/MatthewJDavis/PowerShell/azure-storage-accounts/Azure/storage-account/storage-account-research/storage-acct-parameters-res.json"
 $TestTemplateParameterUri = "https://raw.githubusercontent.com/MatthewJDavis/PowerShell/azure-storage-accounts/Azure/storage-account/storage-account-test/storage-acct-parameters-test.json"
-$DevTemplateParameterUri = "https://raw.githubusercontent.com/MatthewJDavis/PowerShell/azure-storage-accounts/Azure/storage-account/storage-account-test/storage-acct-parameters-test.json"
+$DevTemplateParameterUri = "https://raw.githubusercontent.com/MatthewJDavis/PowerShell/azure-storage-accounts/Azure/storage-account/storage-account-test/storage-acct-parameters-dev.json"
 
 
-New-AzureRmResourceGroupDeployment -Name $deployname -ResourceGroupName $rgname -TemplateUri $TemplateUri -TemplateParameterUri $TemplateParameterUri -Verbose
+New-AzureRmResourceGroupDeployment -Name $deployname -ResourceGroupName $rgname -TemplateUri $TemplateUri -TemplateParameterUri $DevTemplateParameterUri -Verbose
 
 
 
