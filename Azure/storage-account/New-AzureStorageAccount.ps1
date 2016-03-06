9492c0e43faaffe307a5f02b7355ec46c137865e
Matt D <matthewdavis111@gmail.com>
2016-03-06 17:54:37 +0000

    created PowerShell script to call the JSON files in github to create a new storage account in Azure

---
+++ Azure/storage-account/New-AzureStorageAccount.ps1
@@ -0,0 +1,13 @@
+﻿#To get resource groups run> Get-AzureRmResourceGroup 
+#Use raw files fron github
+$deployname = "storageAcctTemplate"
+$rgname = "research-resource-group-Matt"
+$TemplateUri = "https://raw.githubusercontent.com/MatthewJDavis/PowerShell/azure-storage-accounts/Azure/storage-account/storage-account-template/azuredeploy.json"
+$TemplateParameterUri = "https://raw.githubusercontent.com/MatthewJDavis/PowerShell/azure-storage-accounts/Azure/storage-account/storage-account-template/azuredeploy.parameters.json"
+
+
+New-AzureRmResourceGroupDeployment -Name $deployname -ResourceGroupName $rgname -TemplateUri $TemplateUri -TemplateParameterUri $TemplateParameterUri
+
+
+
+
