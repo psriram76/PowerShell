6fb48d043cdf1ad603a2329c95ed646d7ed418de
Matt D <matthewdavis111@gmail.com>
2016-03-07 21:19:18 +0000

    Created new PS file to start a non running service

---
+++ Start-NonRunningService.ps1
@@ -0,0 +1,13 @@
+﻿#Start a non-running service. Can be used as a scheduled task.
+$service = Get-Service -Name Bits
+
+if ($service.Status -ne "Running")
+{
+    $service.Start()
+    Write-EventLog -LogName TestLog -Source TestApp -EntryType Information  -EventId 1 -Message "Service was stopped and needed to be started" -Category 1
+
+}
+else
+{
+    Write-EventLog -LogName TestLog -Source TestApp -EntryType Information  -EventId 0 -Message "Service was running. No action required." -Category 1
+}
