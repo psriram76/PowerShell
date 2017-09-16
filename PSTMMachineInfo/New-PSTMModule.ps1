e5b0ee8d2bccc3438c4097a344054ecdb44e1ad3
= <matthewdavis111@gmail.com>
2017-09-16 21:11:41 +0100

    tidy up of repo

--- PSTMMachineInfo/New-PSTMModule.ps1
+++
@@ -1,1 +0,0 @@
-New-ModuleManifest -Path .\PSTMMachineInfo.psd1 -Author "Matt Davis" -RootModule .\PSTMMachineInfo.psm1 -FunctionsToExport @('Get-TMMachineInfo') -Description "Sample Toolmaking module" -ModuleVersion 1.0.0.0
---
+++ Tools/Get-PhysicalAdapter.ps1
@@ -0,0 +1,40 @@
+﻿<#
+.Synopsis
+Get-PhysicalAdapter retrieves the physical network adapter information from one or more computers.
+.DESCRIPTION
+Get-PhysicalAdapter uses WMI to retrieve the Win32_networkadapter instances from one or more computers. It displays each adapter's MAC address, adapter type,
+Device ID, name and speed.
+.PARAMETER ComputerName
+The computer name or names to query. This is mandatory.
+.EXAMPLE
+Get-PhysicalAdapter -ComputerName localhost
+.EXAMPLE
+Get-PhysicalAdapter -ComputerName server-1
+#>
+function Get-PhysicalAdapter {
+  [CmdletBinding()]
+  Param
+  (
+    [Parameter(Mandatory = $true,
+      ValueFromPipelineByPropertyName = $true,
+      Position = 0)]
+    [Alias('hostname')]
+    [string[]]$ComputerName
+  )
+
+  Begin {
+    Write-Verbose "Connecting to $ComputerName"
+  }
+  Process {
+    Get-WmiObject  win32_networkadapter -ComputerName $ComputerName |
+      Where-Object -FilterScript {$_.PhysicalAdapter} |
+      Select-Object MACAddress, AdapterType, DeviceID, Name, Speed
+  }
+  End {
+    Write-Verbose "Finished running command"
+  }
+}
+
+
+
+
