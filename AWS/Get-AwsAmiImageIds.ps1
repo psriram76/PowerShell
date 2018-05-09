954db44c9846153a68b7a2377c13a8ab58c95367
= <matthewdavis111@gmail.com>
2018-05-08 23:07:45 -0400

    changed to function

--- AWS/Get-AwsAmiImageIds.ps1
+++ AWS/Get-AwsAmiImageIds.ps1
@@ -1,29 +1,39 @@
 #Get the latest Windows AMIs for a specified region
 
-#Region options here
-$region = 'eu-west-1'
+function Get-AWSAmiImageIDs {
+  [CmdletBinding()]
+  #AWS Regions
+  param (
+    [ValidateSet("ap-northeast-1","ap-northeast-2","ap-south-1","ap-southeast-1","ap-southeast-2",
+    "ca-central-1","eu-central-1","eu-west-1","eu-west-2","sa-east-1","us-east-1","us-east-2","us-west-1","us-west-2")]
+    [string]
+    $region = "eu-west-1"
+  )
+  begin {
+    Set-DefaultAWSRegion -Region $region
 
-Set-DefaultAWSRegion -Region $region
+    # array list to hold images
+    $ec2Images = @()
+  }
 
-# array list to hold images
-$ec2Images = @()
+  process {
 
-# EC2 image object
+    $images = Get-EC2ImageByName | Where-Object {$_ -like "WINDOWS_2016*"} 
 
-Write-Output $ec2Image
+    foreach($image in $images){
+      $imageDetails = Get-EC2ImageByName -Name $image
 
-$images = Get-EC2ImageByName | Where-Object {$_ -like "WINDOWS_2016*"} 
+      $ec2Image = New-Object -TypeName psobject
+      $ec2Image | Add-Member -MemberType NoteProperty -Name ImageName -Value $imageDetails.name 
+      $ec2Image | Add-Member -MemberType NoteProperty -Name AmiID -Value $imageDetails.imageid 
+      $ec2Image | Add-Member -MemberType NoteProperty -Name Region -Value $region
 
-foreach($image in $images){
-  $imageDetails = Get-EC2ImageByName -Name $image
+      $ec2Images += $ec2Image
+    }
 
-  
-  $ec2Image = New-Object -TypeName psobject
-  $ec2Image | Add-Member -MemberType NoteProperty -Name ImageName -Value $imageDetails.name 
-  $ec2Image | Add-Member -MemberType NoteProperty -Name AmiID -Value $imageDetails.imageid 
-  $ec2Image | Add-Member -MemberType NoteProperty -Name Region -Value $region
+    Write-Output $ec2Images
+  }
 
-  $ec2Images += $ec2Image
+  end {
+  }
 }
-
-Write-Output $ec2Images
