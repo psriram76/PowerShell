ff67210e4416b004ee2ec7fd31650b8ce9b36161
= <matthewdavis111@gmail.com>
2018-05-08 23:08:22 -0400

    captial param

--- AWS/Get-AwsAmiImageIds.ps1
+++ AWS/Get-AwsAmiImageIds.ps1
@@ -7,10 +7,10 @@ function Get-AWSAmiImageIDs {
     [ValidateSet("ap-northeast-1","ap-northeast-2","ap-south-1","ap-southeast-1","ap-southeast-2",
     "ca-central-1","eu-central-1","eu-west-1","eu-west-2","sa-east-1","us-east-1","us-east-2","us-west-1","us-west-2")]
     [string]
-    $region = "eu-west-1"
+    $Region = "eu-west-1"
   )
   begin {
-    Set-DefaultAWSRegion -Region $region
+    Set-DefaultAWSRegion -Region $Region
 
     # array list to hold images
     $ec2Images = @()
@@ -26,7 +26,7 @@ function Get-AWSAmiImageIDs {
       $ec2Image = New-Object -TypeName psobject
       $ec2Image | Add-Member -MemberType NoteProperty -Name ImageName -Value $imageDetails.name 
       $ec2Image | Add-Member -MemberType NoteProperty -Name AmiID -Value $imageDetails.imageid 
-      $ec2Image | Add-Member -MemberType NoteProperty -Name Region -Value $region
+      $ec2Image | Add-Member -MemberType NoteProperty -Name Region -Value $Region
 
       $ec2Images += $ec2Image
     }
