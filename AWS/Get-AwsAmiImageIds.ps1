f0287fee0f500eb184eb138e741beba448375671
= <matthewdavis111@gmail.com>
2018-05-09 21:45:40 -0400

    added win ver param and try catch

--- AWS/Get-AwsAmiImageIds.ps1
+++ AWS/Get-AwsAmiImageIds.ps1
@@ -7,9 +7,16 @@ function Get-AWSAmiImageIDs {
     [ValidateSet("ap-northeast-1","ap-northeast-2","ap-south-1","ap-southeast-1","ap-southeast-2",
     "ca-central-1","eu-central-1","eu-west-1","eu-west-2","sa-east-1","us-east-1","us-east-2","us-west-1","us-west-2")]
     [string]
-    $Region = "eu-west-1"
-  )
+    $Region = "eu-west-1",
+
+    #Windows Version
+    [ValidateSet('2016', '2012R2', '2012', '2008R2')]
+    [string]
+    $WindowsVersion = '2016'
+)
+
   begin {
+    
     Set-DefaultAWSRegion -Region $Region
 
     # array list to hold images
@@ -18,22 +25,28 @@ function Get-AWSAmiImageIDs {
 
   process {
 
-    $images = Get-EC2ImageByName | Where-Object {$_ -like "WINDOWS_2016*"} 
+    try {
+      $images = Get-EC2ImageByName | Where-Object {$_ -like "WINDOWS_$WindowsVersion*"} 
 
-    foreach($image in $images){
-      $imageDetails = Get-EC2ImageByName -Name $image
+      foreach($image in $images){
+        $imageDetails = Get-EC2ImageByName -Name $image
 
-      $ec2Image = New-Object -TypeName psobject
-      $ec2Image | Add-Member -MemberType NoteProperty -Name ImageName -Value $imageDetails.name 
-      $ec2Image | Add-Member -MemberType NoteProperty -Name AmiID -Value $imageDetails.imageid 
-      $ec2Image | Add-Member -MemberType NoteProperty -Name Region -Value $Region
+        $ec2Image = New-Object -TypeName psobject
+        $ec2Image | Add-Member -MemberType NoteProperty -Name ImageName -Value $imageDetails.name 
+        $ec2Image | Add-Member -MemberType NoteProperty -Name AmiID -Value $imageDetails.imageid 
+        $ec2Image | Add-Member -MemberType NoteProperty -Name Region -Value $Region
 
-      $ec2Images += $ec2Image
+        $ec2Images += $ec2Image
+      }
+
+      Write-Output $ec2Images
     }
-
-    Write-Output $ec2Images
+    catch {
+      Write-Error $_
+    }
   }
 
   end {
+    Clear-DefaultAWSRegion
   }
 }
