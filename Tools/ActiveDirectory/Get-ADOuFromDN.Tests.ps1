4c32ad641b20632634db0514ba99df6bdf77f3eb
= <matthewdavis111@gmail.com>
2018-05-27 22:15:57 -0400

    added ad dn function and tests

---
+++ Tools/ActiveDirectory/Get-ADOuFromDN.Tests.ps1
@@ -0,0 +1,15 @@
+﻿$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
+. "$here\$sut"
+
+Describe "Get-ADOuFromDN" {
+    It "Alerts that OU is not found in the string" {
+        Get-ADOuFromDN -DistinguishedName 'DN=joebloggs,DC=Boss,DC=Com' | Should Be $false 
+    }
+    It "Removes the DN and returns a string starting with OU" {
+        Get-ADOuFromDN -DistinguishedName 'DN=joebloggs,OU=Users,DC=Boss,DC=Com' | Should -eq 'OU=Users,DC=Boss,DC=Com'
+    }
+    It "Returns from the first OU entry" {
+        Get-ADOuFromDN -DistinguishedName 'DN=joebloggs,OU=Users,OU=IT,OU=TopDogs,DC=Boss,DC=Com' | Should -eq 'OU=Users,OU=IT,OU=TopDogs,DC=Boss,DC=Com'
+    }
+}
---
+++ Tools/ActiveDirectory/Get-ADOuFromDN.ps1
@@ -0,0 +1,38 @@
+﻿<#
+.SYNOPSIS
+  Outputs the OU path after removal of the Distinguished Name
+.DESCRIPTION
+  Get a distigguished name property from an Active Directory cmdlet i.e Get-ADUser, Get-ADComputer and this function will return the OU
+  of the object which can be used if you want to create or move other objects to that OU
+.EXAMPLE
+  (Get-ADUser -Identity Keith.Moon).DistinguishedName | Get-ADOuFromDN
+.EXAMPLE
+  (Get-ADComputer dc01).DistinguishedName | Get-ADOuFromDN
+#>
+function Get-ADOuFromDN {
+  [CmdletBinding()]
+  param (
+    # The string with the distinguished name from an AD cmdlet output
+    [Parameter(Mandatory=$true,
+    Position=0,
+    ValueFromPipeline=$true)]
+    [string]
+    $DistinguishedName
+  )
+  
+  begin {
+    if(-not (Select-String -InputObject $DistinguishedName -Pattern 'OU' -CaseSensitive -Quiet)){
+      Write-Host 'OU not found in input string'
+      Break   
+    }
+  }
+  
+  process {
+    $ouIndex = $DistinguishedName.IndexOf('OU')
+    $ouPath = $DistinguishedName.Remove(0,$ouIndex)
+    Write-Output -InputObject $ouPath
+  }
+  
+  end {
+  }
+}
