37063c94691f4f04388e6cb9116c999610295172
= <matthewdavis111@gmail.com>
2018-06-03 19:59:33 -0400

    created adv function

--- Tools/Get-DirectorySize.ps1
+++ Tools/Get-DirectorySize.ps1
@@ -3,27 +3,46 @@
 Displays the amount of MBs used for each subdirectory for a given directory
 .EXAMPLE
 #>
+function Get-DirectorySize {
+  [CmdletBinding()]
+  # Parameter help description
+  param(
+    [Parameter(Mandatory)]
+    [string]
+    $Path
 
-$ParentDirectory = 'C:\Users\Matt\OneDrive\'
-$SubDirectoryList = Get-ChildItem -Path $ParentDirectory -Directory
-$directoryInfoList = @()
-
-foreach ($directory in $SubDirectoryList) {
-  $directoryInfo = [PSCustomObject]@{
-    Name      = $null;
-    Count     = 0;
-    TotalSize = 0;
+  )
+  
+  begin {
   }
-
-  $subDirectory = Get-ChildItem -Path (Join-Path -Path $ParentDirectory -ChildPath $directory.Name) -Recurse
-  $length = $subDirectory | ForEach-Object {$_.Length} | Measure-Object -Sum | Select-Object -Property @{l = "sum"; e = {"$($_.Sum)" / 1MB -as [int]}}
-
-  $directoryInfo.Name = $directory.Name
-  $directoryInfo.Count = $subDirectory.Count
-  $directoryInfo.TotalSize = $length.sum
   
-  $directoryInfoList += $directoryInfo
+  process {
+    
+    $SubDirectoryList = Get-ChildItem -Path $Path -Directory
+    $directoryInfoList = @()
+
+    foreach ($directory in $SubDirectoryList) {
+      $directoryInfo = [PSCustomObject]@{
+        Name      = $null;
+        Count     = 0;
+        TotalSize = 0;
+      }
+
+      $subDirectory = Get-ChildItem -Path (Join-Path -Path $ParentDirectory -ChildPath $directory.Name) -Recurse
+      $length = $subDirectory | ForEach-Object {$_.Length} | Measure-Object -Sum | Select-Object -Property @{l = "sum"; e = {"$($_.Sum)" / 1MB -as [int]}}
+
+      $directoryInfo.Name = $directory.Name
+      $directoryInfo.Count = $subDirectory.Count
+      $directoryInfo.TotalSize = $length.sum
   
+      $directoryInfoList += $directoryInfo
+  
+    }
+
+    Write-Output $directoryInfoList
+  }
+  
+  end {
+  }
 }
 
-Write-Output $directoryInfoList
