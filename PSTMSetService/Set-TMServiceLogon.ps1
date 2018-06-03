61c57d20dbae9f8c799475d065c83b4ea433d56a
= <matthewdavis111@gmail.com>
2018-06-02 23:27:35 -0400

    formatting

--- PSTMSetService/Set-TMServiceLogon.ps1
+++ PSTMSetService/Set-TMServiceLogon.ps1
@@ -8,38 +8,34 @@
 .EXAMPLE
    Another example of how to use this cmdlet
 #>
-function Set-TMServiceLogon
-{
-    [CmdletBinding()]
-    Param
-    (
-        # Param1 help description
-        [Parameter(Mandatory=$true,
-                   ValueFromPipelineByPropertyName=$true,
-                   Position=0)]
-        [String]
-        $ServiceName,
+function Set-TMServiceLogon {
+  [CmdletBinding()]
+  Param
+  (
+    # Param1 help description
+    [Parameter(Mandatory = $true,
+      ValueFromPipelineByPropertyName = $true,
+      Position = 0)]
+    [String]
+    $ServiceName,
 
-        # Param2 help description
-        [String]
-        $NewPassword,
-        [String]
-        $NewUser,
-        [String]
-        $ComputerName
-    )
+    # Param2 help description
+    [String]
+    $NewPassword,
+    [String]
+    $NewUser,
+    [String]
+    $ComputerName
+  )
 
-    Begin
-    {
-    }
-    Process
-    {
-        Invoke-CimMethod -Query "SELECT * FROM win32_service WHERE name = 'Bits'" 
-        -MethodName change 
-        -Arguments @{"StartName" = $NewUser; "StartPassword" = $NewPassword} 
-        -ComputerName $env:COMPUTERNAME
-    }
-    End
-    {
-    }
+  Begin {
+  }
+  Process {
+    Invoke-CimMethod -Query "SELECT * FROM win32_service WHERE name = 'Bits'" 
+    -MethodName change 
+    -Arguments @{"StartName" = $NewUser; "StartPassword" = $NewPassword} 
+    -ComputerName $env:COMPUTERNAME
+  }
+  End {
+  }
 }
--- profile.ps1
+++ profile.ps1
@@ -12,7 +12,7 @@ New-Alias -Name add -Value Add-AzureRmAccount
 
 # add alias to find accelerators
 $TAType = [psobject].Assembly.GetType("System.Management.Automation.TypeAccelerators")
-$TAType::Add('accelerators',$TAType) 
+$TAType::Add('accelerators', $TAType) 
 # [accelerators]::Get
 
 Set-Location -Path C:\git
@@ -23,21 +23,23 @@ $Host.PrivateData.ErrorForegroundColor = 'Cyan'
 Set-PSReadlineKeyHandler -Key Tab -Function Complete
 
 function Prompt {
-    $location = Get-Location
-    $identity = [Security.Principal.WindowsIdentity]::GetCurrent()
-    $principal = [Security.Principal.WindowsPrincipal] $identity
+  $location = Get-Location
+  $identity = [Security.Principal.WindowsIdentity]::GetCurrent()
+  $principal = [Security.Principal.WindowsPrincipal] $identity
 
-    if ($principal.IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
-         $Host.UI.RawUI.WindowTitle = "[ADMIN]: " + $location
-    } else {
-        $Host.UI.RawUI.WindowTitle = $location
-    }
+  if ($principal.IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
+    $Host.UI.RawUI.WindowTitle = "[ADMIN]: " + $location
+  }
+  else {
+    $Host.UI.RawUI.WindowTitle = $location
+  }
 
-    if ($env:ConEmuANSI -eq "ON") {
-    	"PS >"
-    } else {
-        $env:username + " :: " + (Get-Date -format t) + "> " 
-    }
+  if ($env:ConEmuANSI -eq "ON") {
+    "PS >"
+  }
+  else {
+    $env:username + " :: " + (Get-Date -format t) + "> " 
+  }
 }#prompt
 
 
