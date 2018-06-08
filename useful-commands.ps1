9d80af36794c7dcf567218cd5c7ddf47c0deab7e
Matthew Davis <MatthewJDavis@users.noreply.github.com>
2018-06-08 17:49:55 -0400

    Update useful-commands.ps1

--- useful-commands.ps1
+++ useful-commands.ps1
@@ -3,3 +3,14 @@
 # Find the sources that can log to an eventlog
 (Get-ChildItem HKLM:\SYSTEM\CurrentControlSet\Services\EventLog\Application).pschildname
 
+
+# Remove a user / group from directories - must remove inheritance first
+# Remove inheritance
+$acl = Get-Acl -Path $folderName
+$acl.SetAccessRuleProtection($true,$true)
+Set-Acl -Path $folderName -AclObject $acl
+
+# Remove user access
+$acl = Get-Acl -Path $folderName
+$acl.Access | Where-Object {$_.IdentityReference -eq "BUILTIN\Users" } | ForEach-Object { $acl.RemoveAccessRuleSpecific($_) }
+Set-Acl -Path $folderName -AclObject $acl
