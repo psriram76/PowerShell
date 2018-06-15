813e3da0aa872904d470f7234cd65b96f0679a39
Matthew Davis <MatthewJDavis@users.noreply.github.com>
2018-06-14 21:08:35 -0400

    Update vm-set-up-with-ps.ps1

--- vm-set-up-with-ps.ps1
+++ vm-set-up-with-ps.ps1
@@ -1,14 +1,14 @@
 # Set up a vm with PowerShell and choco for PS development
-# Invoke-Webrequest -uir 'https://raw.githubusercontent.com/MatthewJDavis/PowerShell/master/vm-set-up-with-ps.ps1' -UseBasicParsing -OutFile .\set-up.ps1
+# Invoke-Webrequest -uri 'https://raw.githubusercontent.com/MatthewJDavis/PowerShell/master/vm-set-up-with-ps.ps1' -UseBasicParsing -OutFile .\set-up.ps1
 
 # Install choco
 Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
 #Install choco packages
-choco install sysinternals
-choco install vscode
-choco install chrome
-choco install cmder
+choco install sysinternals -y
+choco install vscode -y
+choco install chrome -y
+choco install cmder -y
 
 code --install-extension ms-vscode.PowerShell
 code --install-extenson msazurermtools.azurerm-vscode-tools
