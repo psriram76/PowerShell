540c1b5c70d9b77fe7ca3a7f9bf6f11d518b73d2
= <matthewdavis111@gmail.com>
2018-12-10 01:09:45 +0000

    added restore of user function

--- Tools/ActiveDirectory/Restore-MDADUser.ps1
+++ Tools/ActiveDirectory/Restore-MDADUser.ps1
@@ -1,2 +1,39 @@
 # https://blogs.technet.microsoft.com/canitpro/2016/05/18/powershell-basics-restoring-a-deleted-powershell-user/
-#	$dn = (Get-ADObject -SearchBase (get-addomain).deletedobjectscontainer -IncludeDeletedObjects -filter "samaccountname -eq '$Username'").distinguishedname
+
+function Restore-MDADUser {
+  [CmdletBinding()]
+  param (
+    # The samAccountName of the deleted user
+    [Parameter(Mandatory = $true)]
+    [string]
+    $sAMAccountName
+  )
+
+  begin {
+    $deletedObjectContainer = (get-addomain).DeletedObjectsContainer
+  }
+  
+  process {
+    $distinguishedName = (Get-ADObject -SearchBase $deletedObjectContainer -IncludeDeletedObjects -filter "sAMAccountName -eq '$sAMAccountName'").distinguishedname
+    if ($null -eq $distinguishedName) {
+      Write-Error "sAMAccountName $sAMAccountName is not found in the Active directory recyle bin." -ErrorAction Stop
+    }
+    else {
+      try {
+        Restore-ADObject -Identity $distinguishedName -ErrorAction Stop
+      }
+      catch {
+        Write-Error -Message "Unable to restore $sAMAccount to Active Directory"
+      }
+    }
+
+    $message = "User $sAMAccountName has been restored: `n"
+    $message += (Get-ADUser -Identity $sAMAccountName).DistinguishedName
+    Return $message
+  } # end process
+  
+  end {
+    Remove-Variable deletedObjectContainer
+  }
+}
+
