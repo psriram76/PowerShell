e8867c706381988fdb0d12145040fbc5894b8b4a
= <matthewdavis111@gmail.com>
2018-12-11 00:26:33 +0000

    added find deleted user script

---
+++ Tools/ActiveDirectory/Find-MDADDeletedUser.ps1
@@ -0,0 +1,39 @@
+<#
+.SYNOPSIS
+  Finds a deleted Active Directory User
+.DESCRIPTION
+  Find a deleted Active Directory user in the Active Directory recycle bin.
+.EXAMPLE
+  PS C:\> Find-MDADDeletedUser -UserName test
+
+  This command searches the Active Directory recycle bin for users that sAMAccountName contains the word test anywhere.
+#>
+
+function Find-MDADDeletedUser {
+  [CmdletBinding()]
+  param (
+    # The username of the deleted user
+    [Parameter(Mandatory = $true)]
+    [string]
+    $UserName
+  )
+  
+  begin {
+    # Make sure Active Directory Recycle Bin is Enabled
+    if ($null -eq (Get-ADOptionalFeature -filter *).EnabledScopes) {
+      Write-Error 'Acitve Directory Recycle Bin is not Enabled' -ErrorAction Stop
+    }
+    
+    $deletedObjectContainer = (get-addomain).DeletedObjectsContainer    
+  }
+  
+  process {
+    $userList = Get-ADObject -SearchBase $deletedObjectContainer -IncludeDeletedObjects -filter "sAMAccountName -like '*$UserName*'" -Properties userPrincipalName, sAMAccountName  |
+      Select-Object userPrincipalName, sAMAccountName
+    Write-Output $userList
+  }
+  
+  end {
+    remove-variable deletedObjectContainer
+  }
+}
