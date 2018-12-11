275ec906c31ef60189e723f7fff74b792ef74b30
= <matthewdavis111@gmail.com>
2018-12-11 01:41:24 +0000

    formatting

--- Tools/MDActiveDirectory/MDActiveDirectory/Public/New-MDADUser.ps1
+++ Tools/MDActiveDirectory/MDActiveDirectory/Public/New-MDADUser.ps1
@@ -1,18 +1,18 @@
 #Simple script to create an AD user
 
 param(
-# Parameter help description
-[Parameter(Mandatory = $true)]
-[String]
-$FirstName,
-# Parameter help description
+  # Parameter help description
   [Parameter(Mandatory = $true)]
-[String]
-$Surname,
-# Parameter help description
+  [String]
+  $FirstName,
+  # Parameter help description
   [Parameter(Mandatory = $true)]
-[SecureString]
-$Password
+  [String]
+  $Surname,
+  # Parameter help description
+  [Parameter(Mandatory = $true)]
+  [SecureString]
+  $Password
 )
 
 $displayName = "$FirstName $Surname"
@@ -20,20 +20,20 @@ $path = ',OU=Users,DC=example,DC=com'
 $description = 'Example of creating a user'
 $domain = 'example.com'
 $samAcctName = ("$FirstName.$Surname").ToLower()
-$upn =  "$samAcctName@$domain"
+$upn = "$samAcctName@$domain"
 
 $params = @{
-    'Name' = $displayName;
-    'GivenName' = $FirstName;
-    'surname' = $Surname;
-    'DisplayName' = $displayName;
-    'Path' = $path;
-    'Description' = $description;
-    'UserPrincipalName' = $upn;
-    'SamAccountName' = $samAcctName;
-    'AccountPassword' = $Password;
-    'ChangePasswordAtLogon' = $false;
-    'Enabled' = $true
+  'Name'                  = $displayName;
+  'GivenName'             = $FirstName;
+  'surname'               = $Surname;
+  'DisplayName'           = $displayName;
+  'Path'                  = $path;
+  'Description'           = $description;
+  'UserPrincipalName'     = $upn;
+  'SamAccountName'        = $samAcctName;
+  'AccountPassword'       = $Password;
+  'ChangePasswordAtLogon' = $false;
+  'Enabled'               = $true
 }
 
 
