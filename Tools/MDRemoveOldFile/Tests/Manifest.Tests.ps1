7b36aab9e12d8199e67e996aa12d4db06848735c
= <matthewdavis111@gmail.com>
2019-04-21 21:40:19 -0400

    added manifest test

---
+++ Tools/MDRemoveOldFile/Tests/Manifest.Tests.ps1
@@ -0,0 +1,26 @@
+$ModuleName = 'Remove-MDOldFile'
+$FunctionName = 'Remove-MDOldFile'
+
+$ProjectRoot = Resolve-Path "$PSScriptRoot\.."
+$Root = Resolve-Path "$projectRoot\$ModuleName"
+
+Get-Module $ModuleName | Remove-Module -Force
+
+$ModuleInformation = Import-Module $Root\$ModuleName.psm1 -Force -PassThru
+
+$ExportedFunctions = $ModuleInformation.ExportedFunctions.Values.Name
+
+Describe "$ModuleName Module - Testing Manifest File (.psd1)" {
+    Context 'Manifest' {
+        It "Should have an author" {
+            $ModuleInformation.Author | Should not BeNullOrEmpty
+        }
+        It "Should export 1 function" {
+            $ExportedFunctions.count | Should Be 1
+        }
+        It "Should export $FunctionName function" {
+            $ExportedFunctions | Should Be $FunctionName 
+        }
+    }
+}
+
