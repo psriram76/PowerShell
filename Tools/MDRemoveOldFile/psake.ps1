2637a3641d760c683cb02550aeaab3f5ab805c67
= <matthewdavis111@gmail.com>
2019-04-21 21:17:39 -0400

    added test and deploy to build

--- Tools/MDRemoveOldFile/psake.ps1
+++ Tools/MDRemoveOldFile/psake.ps1
@@ -8,3 +8,17 @@ task AnalyseFunction {
         Write-Error -Message 'One or more script analyser errors/ warnings were found'
     }
 }
+
+task RunTests {
+    'Running Pester test on the function'
+    $TestResults = Invoke-Pester -Path $PSScriptRoot\Tests\*.Tests.ps1 -PassThru
+    if ($TestResults.FailedCount -gt 0) {
+        $TestResults | Format-Table
+        Write-Error -Message 'One or more pester tests failed.'
+    }
+}
+
+task DeployModule -depends AnalyseFunction, RunTests {
+    'Deploying module'
+    Copy-Item $PSScriptRoot\Remove-MDOldFile -Destination C:\TEMP -Recurse -Force
+}
