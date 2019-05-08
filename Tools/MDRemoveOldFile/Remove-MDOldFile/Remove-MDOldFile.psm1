7b47e3bac0c6200697cc7877de3b0806c040c8cb
= <matthewdavis111@gmail.com>
2019-05-07 21:54:10 -0400

    added help

--- Tools/MDRemoveOldFile/Remove-MDOldFile/Remove-MDOldFile.psm1
+++ Tools/MDRemoveOldFile/Remove-MDOldFile/Remove-MDOldFile.psm1
@@ -1,4 +1,23 @@
 function Remove-MDOldFile {
+    <#
+    .SYNOPSIS
+        Removes files that have a write time older than the passed in amount of days
+    .DESCRIPTION
+        Removes files with the specified extension that are older than (last write time) the number of days to keep that in input to the function.
+    .EXAMPLE
+        PS C:\> Remove-MDOldFile -Path C:\test -NumberOfDaysToKeep 10 -FileType txt
+        Removes all txt files in the C:\test directory that were written to over 10 days ago
+        
+        PS C:\> Remove-MDOldFile -Path C:\test -NumberOfDaysToKeep 20 -FileType etl
+        Removes all etl files in the C:\test directory that were written over 20 days ago
+
+        PS C:\> Remove-MDOldFile -Path C:\test -NumberOfDaysToKeep 5 -FileType log
+        Removes all log files in the C:\test directory that were written over 5 days ago
+
+    .INPUTS
+    .OUTPUTS
+    .NOTES
+    #>
     [CmdletBinding(
         SupportsShouldProcess = $True
     )]
@@ -25,7 +44,7 @@ function Remove-MDOldFile {
     
     process {
         if (Test-Path $Path) {
-            Get-ChildItem -Path $Path | Where-Object -FilterScript { $_.LastWriteTime -LT (Get-Date).AddDays(-$NumberOfDaysToKeep) -and $_.Name -like "*.$FileType" }|
+            Get-ChildItem -Path $Path | Where-Object -FilterScript { $_.LastWriteTime -LT (Get-Date).AddDays(-$NumberOfDaysToKeep) -and $_.Name -like "*.$FileType" } |
             Remove-Item -Force 
         } else {
             Write-Warning "Path not found"
