9ba7b5fe254c7c000fac0f98cf9fcfaa880c58b4
MatthewJDavis <matthewdavis111@gmail.com>
2019-08-19 21:05:47 -0400

    update ct script

--- AWS/New-MDAllRegionCloudTrail.ps1
+++ AWS/New-MDAllRegionCloudTrail.ps1
@@ -1,11 +1,22 @@
+<#
+.SYNOPSIS
+    Create an AWS multiregion CloudTrail and S3 bucket logging all data events for S3 and lambda services.
+.DESCRIPTION
+    This script creates an S3 bucket with public access blocked for CloudTrail logs. 
+    The CloudTrail created is a multiregion trail that logs all data events for S3 and Lambda.
+    An IAM user or role is required to have permissions to create a CloudTrail and S3 bucket.
+.NOTES
+    General notes
+#>
+
 $AccountName = Get-IAMAccountAlias
 $AccountNumber = (Get-STSCallerIdentity).account
 $BucketName = "cloudtrail-$AccountNumber"
 $CloudTrailName = 'matt-cloudtrail'
-$CloudTrailName = 'matt-cloudtrail'
 
-# create bucket for cloudtrail
+#region S3Bucket
 
+# Bucket Policy to allow CloudTrail to write logs to the bucket
 $S3CloudTrailPolicy = @"
 {
     "Version": "2012-10-17",
@@ -38,63 +49,61 @@ $S3CloudTrailPolicy = @"
 "@
 
 
-# Create S3 bucket
+# Create S3 bucket and apply the bucket policy
 New-S3Bucket -BucketName $BucketName
-
 Write-S3BucketPolicy -BucketName $BucketName -Policy $S3CloudTrailPolicy
 
+# Block public access for the bucket and objects
 $s3PublicParams = @{
-    BucketName = $BucketName
-    PublicAccessBlockConfiguration_BlockPublicAcl = $true
-    PublicAccessBlockConfiguration_BlockPublicPolicy = $true
-    PublicAccessBlockConfiguration_IgnorePublicAcl =  $true
+    BucketName                                          = $BucketName
+    PublicAccessBlockConfiguration_BlockPublicAcl       = $true
+    PublicAccessBlockConfiguration_BlockPublicPolicy    = $true
+    PublicAccessBlockConfiguration_IgnorePublicAcl      = $true
     PublicAccessBlockConfiguration_RestrictPublicBucket = $true
 }
 
 Add-S3PublicAccessBlock @s3PublicParams
+#endregion S3Bucket
 
-
-# Create cloudtrail
-
+#region CloudTrail
 $params = @{
-    Name=$CloudTrailName
-    S3BucketName=$BucketName
-    EnableLogFileValidation = $true
-    IsMultiRegionTrail = $true
+    Name                      = $CloudTrailName
+    S3BucketName              = $BucketName
+    EnableLogFileValidation   = $true
+    IsMultiRegionTrail        = $true
     IncludeGlobalServiceEvent = $true
 }
 
+# Create trail
 New-CTTrail @params
 
+# Create description tag and add to trail
 $tag = New-Object -TypeName Amazon.CloudTrail.Model.Tag
 $tag.Key = 'Description'
 $tag.Value = 'All region Cloudtrail logging all data events for S3 and Lambda.'
 
 Add-CTTag -ResourceId  (Get-CTTrail -TrailNameList $CloudTrailName).TrailARN -TagsList $tag
 
-# create event selector
-
+# Create CloudTrail event selector object
 $EventSelector = New-Object -TypeName Amazon.CloudTrail.Model.EventSelector
-
 $EventSelector.IncludeManagementEvents = $true
 $EventSelector.ReadWriteType = 'All'
 
-
+# S3 data resource
 $S3DataResource = New-Object -TypeName Amazon.CloudTrail.Model.DataResource 
 $S3DataResource.Type = 'AWS::S3::Object'
 $S3DataResource.Values = 'arn:aws:s3'
 
+# Lambda data resource
 $LambdaDataResource = New-Object -TypeName Amazon.CloudTrail.Model.DataResource 
 $LambdaDataResource.Type = 'AWS::Lambda::Function'
 $LambdaDataResource.Values = 'arn:aws:lambda'
 
+# Add data resources to event selector and update CloudTrail
 $EventSelector.DataResources = $S3DataResource, $LambdaDataResource
-
 Write-CTEventSelector -TrailName $CloudTrailName -EventSelector $EventSelector
 
-
+# Start CloudTrail logging
 Start-CTLogging -Name $CloudTrailName
-
-
-Set-ClipboardText $AccountName
+#endregion CloudTrail
 
