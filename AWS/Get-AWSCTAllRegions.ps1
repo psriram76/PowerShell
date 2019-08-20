bf0c7819d58c748857bbddec675ac30e288d9db7
MatthewJDavis <matthewdavis111@gmail.com>
2019-08-19 20:53:00 -0400

    update script

--- AWS/Get-AWSCTAllRegions.ps1
+++ AWS/Get-AWSCTAllRegions.ps1
@@ -1,10 +1,21 @@
-# get -trails
+<#
+.SYNOPSIS
+    Find all CloudTrails in all AWS regions and remove them if needed
+.DESCRIPTION
+    Iterate over all regions and output any cloudtrail found with the specified name.
+    Handy if cloudtrails were created before a multiregion trail was available and used to remove old trails
+    and set up a single multiregion trail.
+#>
 
-foreach($region in $RegionList){
-    Get-CTTrail -Region $region.region | Where-Object {$_.name -eq 'Default'}
+$RegionList = Get-AWSRegion
+$CloudTrailName = 'Default'
+
+
+foreach ($region in $RegionList) {
+    Get-CTTrail -Region $region.region | Where-Object { $_.name -eq $CloudTrailName }
 }
 
-# Remove all cloudtrails
-foreach($region in $RegionList){
-    Get-CTTrail -Region $region.region | Where-Object {$_.name -eq 'Default'} | Remove-CTTrail -Force -Region $region.region
+# Remove all cloudtrails whatif added for safety - remove WhatIf to remove the trails.
+foreach ($region in $RegionList) {
+    Get-CTTrail -Region $region.region | Where-Object { $_.name -eq $CloudTrailName } | Remove-CTTrail -Force -Region $region.region -WhatIf
 }
