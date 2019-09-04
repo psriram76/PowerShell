fec5dcce5732b4f342639c56457cbbeb283333c7
Matthew Davis <MatthewJDavis@users.noreply.github.com>
2019-09-03 20:35:26 -0400

    Update New-MDADUser.ps1

--- Tools/MDActiveDirectory/MDActiveDirectory/Public/New-MDADUser.ps1
+++ Tools/MDActiveDirectory/MDActiveDirectory/Public/New-MDADUser.ps1
@@ -1,40 +1,43 @@
 #Simple script to create an AD user
+function New-MDADUser {
 
-param(
-  # Parameter help description
-  [Parameter(Mandatory = $true)]
-  [String]
-  $FirstName,
-  # Parameter help description
-  [Parameter(Mandatory = $true)]
-  [String]
-  $Surname,
-  # Parameter help description
-  [Parameter(Mandatory = $true)]
-  [SecureString]
-  $Password
-)
+    [CmdletBinding()]
+    param(
+      # Parameter help description
+      [Parameter(Mandatory = $true)]
+      [String]
+      $FirstName,
+      # Parameter help description
+      [Parameter(Mandatory = $true)]
+      [String]
+      $Surname,
+      # Parameter help description
+      [Parameter(Mandatory = $true)]
+      [SecureString]
+      $Password
+    )
 
-$displayName = "$FirstName $Surname"
-$path = ',OU=Users,DC=example,DC=com'
-$description = 'Example of creating a user'
-$domain = 'example.com'
-$samAcctName = ("$FirstName.$Surname").ToLower()
-$upn = "$samAcctName@$domain"
+    $displayName = "$FirstName $Surname"
+    $path = ',OU=Users,DC=example,DC=com'
+    $description = 'Example of creating a user'
+    $domain = 'example.com'
+    $samAcctName = ("$FirstName.$Surname").ToLower()
+    $upn = "$samAcctName@$domain"
 
-$params = @{
-  'Name'                  = $displayName;
-  'GivenName'             = $FirstName;
-  'surname'               = $Surname;
-  'DisplayName'           = $displayName;
-  'Path'                  = $path;
-  'Description'           = $description;
-  'UserPrincipalName'     = $upn;
-  'SamAccountName'        = $samAcctName;
-  'AccountPassword'       = $Password;
-  'ChangePasswordAtLogon' = $false;
-  'Enabled'               = $true
+    $params = @{
+      'Name'                  = $displayName;
+      'GivenName'             = $FirstName;
+      'surname'               = $Surname;
+      'DisplayName'           = $displayName;
+      'Path'                  = $path;
+      'Description'           = $description;
+      'UserPrincipalName'     = $upn;
+      'SamAccountName'        = $samAcctName;
+      'AccountPassword'       = $Password;
+      'ChangePasswordAtLogon' = $false;
+      'Enabled'               = $true
+    }
+
+
+    New-ADUser @params
 }
-
-
-New-ADUser @params
