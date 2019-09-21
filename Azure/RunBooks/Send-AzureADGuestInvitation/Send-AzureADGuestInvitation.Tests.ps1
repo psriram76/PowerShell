77fb99e40e2d5be748acdf5f01bb947c49a867dc
MatthewJDavis <matthewdavis111@gmail.com>
2019-09-21 18:54:39 -0400

    added blank functions

--- Azure/RunBooks/Send-AzureADGuestInvitation/Send-AzureADGuestInvitation.Tests.ps1
+++ Azure/RunBooks/Send-AzureADGuestInvitation/Send-AzureADGuestInvitation.Tests.ps1
@@ -1,4 +1,10 @@
 $here = Split-Path -Parent $MyInvocation.MyCommand.Path
 $sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
 
+# Create empty functions for cmdlets not on system to be mocked later
+  function New-AzureADMSInvitation {}
+  function Add-AzureADGroupMember {}
+  function Get-AzureADUser {}
 
+
+  
